ce3e7d0e1dd6bf578d169227e8d92b7e4b71669f
hoiget <112142799+hoiget@users.noreply.github.com>
2022-10-19 13:34:39 +0700

    Delete BaiTapLon directory

--- BaiTapLon/js/payment.js
+++
@@ -1,9 +0,0 @@
-import { username } from './auth.js';
-let orderDetail = JSON.parse(sessionStorage.getItem('cart'));
-let data = {
-	user: username,
-	address: 'Đại học công nghiệp thành phố hồ chí minh',
-	orderDetails: orderDetail,
-};
-
-console.log(data);
--- BaiTapLon/js/url.js
+++
@@ -1,4 +0,0 @@
-// let url = 'http://localhost:8000';
-let url = 'https://ecommerce-backend-cc.herokuapp.com';
-
-export default url;
